235680a53b43c45193f8ffd2eb3f43335e83c4cc
Mike Hodnick <mike@kindohm.com>
2025-08-15 09:46:11 -0500

    no secrets

--- src/lib/getApp.ts
+++ src/lib/getApp.ts
@@ -3,13 +3,13 @@ import { FirebaseApp, initializeApp } from "firebase/app";
 let app: FirebaseApp;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmjZBhcxzJ9Dpdqj2elAL_vxzobjsUDls",
-  authDomain: "tier-vote.firebaseapp.com",
-  databaseURL: "https://tier-vote-default-rtdb.firebaseio.com",
-  projectId: "tier-vote",
-  storageBucket: "tier-vote.appspot.com",
-  messagingSenderId: "933088926660",
-  appId: "1:933088926660:web:86b38f3d01c0a0b3e82291",
+  apiKey: "asdf",
+  authDomain: "asdf",
+  databaseURL: "asdf",
+  projectId: "asdf",
+  storageBucket: "asdf",
+  messagingSenderId: "asdf",
+  appId: "asdf",
 };
 
 export const getApp = () => {
