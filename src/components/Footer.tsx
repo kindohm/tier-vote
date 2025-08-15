851f1a7782096c38ce966f3be188a7ebae39b9a6
Mike Hodnick <mike@kindohm.com>
2025-08-15 15:40:13 -0500

    enhance footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,23 @@
 export const Footer = () => {
   return (
-    <footer className="mt-5" style={{ fontSize: "75%" }}>
+    <footer className="mt-5 mb-5" style={{ fontSize: "75%" }}>
       <small>
-        Made by Mike Hodnick. Unauthorized duplication may result in sudden
-        death or dismemberment.
+        Made by{" "}
+        <a href="https://kindohm.com" target="_blank">
+          Mike Hodnick
+        </a>
+        . Unauthorized duplication may result in sudden death or dismemberment.{" "}
+        <a
+          href="https://github.com/kindohm/tier-vote/issues"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Submit a bug or idea.
+        </a>{" "}
+        <a href="https://github.com/kindohm/tier-vote/pulls">
+          Awaiting your PR
+        </a>
+        .
       </small>
     </footer>
   );
